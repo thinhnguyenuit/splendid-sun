fa4f4b5f2cb84e1afc2ffd0f942b7b580e0e913d
Thinh Nguyen <dangthinhvh@gmail.com>
2022-05-21 20:48:40 +0700

    Squashed commit of the following:
    commit 9ccb76fa3c2cc86183cba91e01305842025e5cf7
    Author: Thinh Nguyen <dangthinhvh@gmail.com>
    Date:   Sat May 21 20:43:45 2022 +0700
    
        Fix heroku uri
    
    commit 1418df6a5ef76dcb656653f03221816fed6090ae
    Author: Thinh Nguyen <dangthinhvh@gmail.com>
    Date:   Sat May 21 20:38:24 2022 +0700
    
        Trigger Heroku deploy
    
    commit ca0b77162aa7c5b5e74bb89ec29c877ed1d9e388
    Merge: 6226faa 62b9e4e
    Author: Thinh <38901129+thinhnguyenuit@users.noreply.github.com>
    Date:   Tue May 17 21:32:01 2022 +0700
    
        Merge pull request #2 from thinhnguyenuit/develop
    
        Release 1.0.0
    
    commit 6226faaedb2e18975509e675f7e143ccd873aa32
    Merge: 5418fda c693534
    Author: Thinh <38901129+thinhnguyenuit@users.noreply.github.com>
    Date:   Wed Apr 13 20:58:01 2022 +0700
    
        Merge pull request #1 from thinhnguyenuit/develop
    
        Init and add github action workflows

--- app/app.py
+++ app/app.py
@@ -22,7 +22,10 @@ def configure_app(app: Flask) -> None:
     load_dotenv()
 
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DATABASE_URL")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     app.register_blueprint(core)
